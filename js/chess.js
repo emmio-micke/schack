2560e5e08fc1168209af5f30e170df34724a7175
Mikael Olsson <mikael.olsson@emmio.se>
2019-10-29 11:30:06 +0100

    Objectified game and setup pieces

--- js/chess.js
+++ js/chess.js
@@ -1,3 +1,66 @@
+class Game {
+    constructor() {
+        this.turn = null;
+        this.pieces_black = [];
+        this.pieces_white = [];
+        this.board = null;
+
+        this.drawGameBoard();
+        this.setupPieces();
+    }
+
+    drawGameBoard() {
+        let columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    
+        for (let y = 8; y >= 1; y--) {
+            let cell_class = y % 2 == 0 ? "cellBlack" : "cellWhite";
+            for (let x = 0; x <= 7; x++) {
+                cell_class = cell_class == "cellBlack" ? "cellWhite" : "cellBlack";
+                $( "<div>", {
+                    "class": "cell " + cell_class,
+                    id: columns[x] + y
+                }).appendTo(".container");
+            }
+        }
+    }
+
+    setupPieces() {
+        this.pieces_white.push(new Piece ("rook", "b", "A8"));
+        this.pieces_white.push(new Piece ("knight", "b", "B8"));
+        this.pieces_white.push(new Piece ("bishop", "b", "C8"));
+        this.pieces_white.push(new Piece ("queen", "b", "D8"));
+        this.pieces_white.push(new Piece ("king", "b", "E8"));
+        this.pieces_white.push(new Piece ("bishop", "b", "F8"));
+        this.pieces_white.push(new Piece ("knight", "b", "G8"));
+        this.pieces_white.push(new Piece ("rook", "b", "H8"));
+        this.pieces_white.push(new Piece ("pawn", "b", "A7"));
+        this.pieces_white.push(new Piece ("pawn", "b", "B7"));
+        this.pieces_white.push(new Piece ("pawn", "b", "C7"));
+        this.pieces_white.push(new Piece ("pawn", "b", "D7"));
+        this.pieces_white.push(new Piece ("pawn", "b", "E7"));
+        this.pieces_white.push(new Piece ("pawn", "b", "F7"));
+        this.pieces_white.push(new Piece ("pawn", "b", "G7"));
+        this.pieces_white.push(new Piece ("pawn", "b", "H7"));
+
+        this.pieces_white.push(new Piece ("pawn", "w", "A2"));
+        this.pieces_white.push(new Piece ("pawn", "w", "B2"));
+        this.pieces_white.push(new Piece ("pawn", "w", "C2"));
+        this.pieces_white.push(new Piece ("pawn", "w", "D2"));
+        this.pieces_white.push(new Piece ("pawn", "w", "E2"));
+        this.pieces_white.push(new Piece ("pawn", "w", "F2"));
+        this.pieces_white.push(new Piece ("pawn", "w", "G2"));
+        this.pieces_white.push(new Piece ("pawn", "w", "H2"));
+        this.pieces_white.push(new Piece ("rook", "w", "A1"));
+        this.pieces_white.push(new Piece ("knight", "w", "B1"));
+        this.pieces_white.push(new Piece ("bishop", "w", "C1"));
+        this.pieces_white.push(new Piece ("queen", "w", "D1"));
+        this.pieces_white.push(new Piece ("king", "w", "E1"));
+        this.pieces_white.push(new Piece ("bishop", "w", "F1"));
+        this.pieces_white.push(new Piece ("knight", "w", "G1"));
+        this.pieces_white.push(new Piece ("rook", "w", "H1"));
+    }
+}
+
 class Piece {
     constructor(type, color, position) {
         this.type = type;
@@ -7,6 +70,7 @@ class Piece {
         let pos_y = position.substring(1);
         this.position_x = pos_x;
         this.position_y = pos_y;
+
         this.image = "images/" + color + "_" + type + ".png";
 
         this.placePiece();
@@ -21,23 +85,7 @@ class Piece {
     }    
 }
 
-function drawGameBoard() {
-    let columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-
-    for (let y = 8; y >= 1; y--) {
-        let cell_class = y % 2 == 0 ? "cellBlack" : "cellWhite";
-        for (let x = 0; x <= 7; x++) {
-            cell_class = cell_class == "cellBlack" ? "cellWhite" : "cellBlack";
-            $( "<div>", {
-                "class": "cell " + cell_class,
-                id: columns[x] + y
-            }).appendTo(".container");
-        }
-    }
-}
 
 $(document).ready(function () {
-    drawGameBoard();
-    let piece1 = new Piece ("queen", "w", "D1");
-    let piece2 = new Piece ("king", "w", "E1");
+    let game = new Game();
 });
