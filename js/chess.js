3877cb53e1f81e520c433a6b36a2dba858570948
Mikael Olsson <mikael.olsson@emmio.se>
2019-10-29 09:37:49 +0100

    Fixed temporary gameboard.

--- js/chess.js
+++ js/chess.js
@@ -2,8 +2,9 @@ function drawGameBoard() {
     let columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
     for (let y = 8; y >= 1; y--) {
-        let cell_class = y % 2 == 0 ? "cellBlack" : "cellBlack";
+        let cell_class = y % 2 == 0 ? "cellBlack" : "cellWhite";
         for (let x = 0; x <= 7; x++) {
+            cell_class = cell_class == "cellBlack" ? "cellWhite" : "cellBlack";
             $( "<div>", {
                 "class": "cell " + cell_class,
                 id: columns[x] + y
