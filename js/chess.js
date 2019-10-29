94987eb2c02d2ca48b4b3d321dd355b5c2bac778
Mikael Olsson <mikael.olsson@emmio.se>
2019-10-29 11:11:12 +0100

    Objectified pieces.

--- js/chess.js
+++ js/chess.js
@@ -1,3 +1,26 @@
+class Piece {
+    constructor(type, color, position) {
+        this.type = type;
+        this.color = color;
+
+        let pos_x = position.substring(0, 1);
+        let pos_y = position.substring(1);
+        this.position_x = pos_x;
+        this.position_y = pos_y;
+        this.image = "images/" + color + "_" + type + ".png";
+
+        this.placePiece();
+    }
+
+    placePiece() {
+        let img_position = "#" + this.position_x + this.position_y;
+    
+        $( "<img>", {
+            "src": this.image
+        }).appendTo(img_position);
+    }    
+}
+
 function drawGameBoard() {
     let columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
@@ -13,17 +36,8 @@ function drawGameBoard() {
     }
 }
 
-function placePiece(piece, color, position) {
-    let image = "images/" + color + "_" + piece + ".png";
-
-    let img_position = "#" + position;
-
-    $( "<img>", {
-        "src": image
-    }).appendTo(img_position);
-}
-
 $(document).ready(function () {
     drawGameBoard();
-    placePiece("queen", "w", "D1");
+    let piece1 = new Piece ("queen", "w", "D1");
+    let piece2 = new Piece ("king", "w", "E1");
 });
